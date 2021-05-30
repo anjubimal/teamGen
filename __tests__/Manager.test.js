f51259b9eaad94a52c83db0be5ce5976f392da2d
anjubimal <anjubimal@tamu.edu>
2021-05-29 22:58:51 -0400

    manager and intern js files added

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require("../lib/Manager")
+
+test("Can set office number", () => {
+    const testValue = 100;
+    const employee = new Manager("snowy", 1, "test@test.com", testValue);
+    expect(employee.office).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const employee = new Manager("snowy", 1, "test@test.com");
+    expect(employee.getRole()).toBe(testValue);
+});
